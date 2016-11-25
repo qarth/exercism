518830fd4fc87635eccc5e892665075e209d4db4
Robbie Wright <a1210993@gmail.com>
2016-11-25 16:18:27 +1030

    Merge https://github.com/qarth/exercism
    # Conflicts:
    #	javascript/hello-world/README.md
    #	javascript/hello-world/hello-world.js
    #	javascript/hello-world/hello-world.spec.js
    #	javascript/leap/README.md
    #	javascript/leap/leap.js
    #	javascript/leap/leap.spec.js

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,20 +1,3 @@
- * Created by root on 25/11/2016.
- */
-var Year;
-Year = function (year) {
-    this.year = year;
-};
-
-Year.prototype.isLeap = function() {
-    var year = this.year;
-    if (year % 400 === 0) {
-        return true;
-    } else if (year % 100 === 0) {
-        return false;
-    } else return year % 4 === 0;
-};
-
-=======
 /**
  * Created by root on 25/11/2016.
  */
